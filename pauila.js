977309f6887d247c6b57d450489d1f0845ea4abd
Sebas <dev_sebasfc@outlook.es>
2022-02-15 18:57:10 -0500

    new actl

--- pauila.js
+++
@@ -1,6 +0,0 @@
-var paula = [Monedero: 0, anno : 8];
-
-function Ganardinero(a){
-    paula.Monedero = a
-    return console.log(paula);
-};
--- paula.js
+++
@@ -1,8 +0,0 @@
-var paula = [Monedero: 0, anno : 8];
-
-function perderdinero(a){
-    paula.Monedero = a
-    return console.log(pn);
-};
-
-//paula tontina
